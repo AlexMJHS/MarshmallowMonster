bb37a0a8d07ada26fd07e4ebbed3edd8c8e5d1e5
Alejandro <AlexMJHS@gmail.com>
2015-09-11 09:02:56 -0600

    Added extra monster info
    Added more information to the toString

--- src/marshmallows/model/MarshmallowMonster.java
+++ src/marshmallows/model/MarshmallowMonster.java
@@ -26,7 +26,7 @@ public class MarshmallowMonster
 	}
 	public String toString()
 	{
-		String monster = "This monster has " + monsterEyes + " eyes, and it's name is " + monsterName;
+		String monster = "This monster has " + monsterEyes + " eyes, and it's name is " + monsterName + "." + " It only has " + monsterHair + " strands of hair and " + monsterLegs + " legs";
 		
 		return monster;
 	}
